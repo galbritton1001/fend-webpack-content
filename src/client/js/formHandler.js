c72910c56891ad3798d15d3dbcfc3657aaf68dbf
GregA <77366245+galbritton1001@users.noreply.github.com>
2021-03-08 10:41:56 -0600

    new event

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,16 +1,20 @@
-function handleSubmit(event) {
+async function handleSubmit(event) {
     event.preventDefault()
-
-    // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    checkForName(formText)
-
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+        const myurl = `https://api.meaningcloud.com/sentiment-2.1?key=f450e24343025f4e5ce1aa0c97f6cd1d&of=json&txt=Main%20dishes%20were%20quite%20good%2C%20but%20desserts%20were%20too%20sweet%20for%20me.&model=general&lang=en`;
+        //get weather data from api
+        const response = await fetch(myurl);
+        try {
+          //const myweather = await response.json();
+          //const ztemp = myweather.main.temp;
+          //const zdate = dateTimeStamp();
+          //const ztext = document.getElementById("feelings").value;
+          //const mydata = { ztemp, zdate, ztext };
+          //sendToServer(mydata);
+        } catch (error) {
+          console.log("error", error);
+        }
+      
 }
+
 
 export { handleSubmit }
